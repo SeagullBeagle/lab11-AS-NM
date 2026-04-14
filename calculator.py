e59b05f47951ec6b5337c13c1a6511f76bd5064a
SeagullBeagle <nineisland2410@gmail.com>
2026-04-13 20:19:55 -0400

    Implement basic calculator functions

--- calculator.py
+++ calculator.py
@@ -4,8 +4,52 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+# https://github.com/SeagullBeagle/lab11-AS-NM
+# Partner 1: Adam Siegel 
+# Partner 2: Nicolas Murguia 
+
+import math 
+
+def square_root(a):
+    if a < 0:
+        raise ValueError("bad input")
+    x = math.sqrt(a)
+    return x
+
+def hypotenuse(a, b):
+    h = math.hypot(a, b)
+    return h
+
+def add(a, b):
+    c = a + b
+    return c
+
+def subtract(a, b):
+    c = a - b
+    return c
+
+def multiply(a, b):
+    c = a * b
+    return c
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("can't divide by 0")
+    x = b / a
+    return x
+
+def logarithm(a, b):
+    if a <= 0 or a == 1:
+        raise ValueError("bad base")
+    if b <= 0:
+        raise ValueError("bad num")
+    x = math.log(b, a)
+    return x
+
+def exponent(a, b):
+    x = a ** b
+    return x
+    
+    
 
 
